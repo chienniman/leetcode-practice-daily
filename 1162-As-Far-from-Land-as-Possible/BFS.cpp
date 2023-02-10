98dc04daad2580da9f8707d4547c4fbb731e163e
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-02-10 22:18:04 +0800

    [create]引入&新增

---
+++ 1162-As-Far-from-Land-as-Possible/BFS.cpp
@@ -0,0 +1,15 @@
+// C++ Queue of Pairs FIFOS
+// Queue<pair<datatype,datatype>>queue_od_pairs
+#include <vector>
+#include <queue>
+#include <utility>
+#include <algorithm>
+
+using namespace std;
+
+class Solution {
+public:
+    int maxDistance(vector<vector<int>>& grid) {
+        
+    }
+};
