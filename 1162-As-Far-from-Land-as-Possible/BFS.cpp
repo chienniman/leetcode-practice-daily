3ef43fc486334cda1b7236d0f0605611baa8e589
齊尼曼 <97031067+chienniman@users.noreply.github.com>
2023-02-11 11:57:16 +0800

    [test]新增test case

--- 1162-As-Far-from-Land-as-Possible/BFS.cpp
+++ 1162-As-Far-from-Land-as-Possible/BFS.cpp
@@ -1,5 +1,4 @@
-// C++ Queue of Pairs FIFOS
-// Queue<pair<datatype,datatype>>queue_od_pairs
+#include <iostream>
 #include <vector>
 #include <queue>
 #include <utility>
@@ -7,9 +6,23 @@
 
 using namespace std;
 
-class Solution {
-public:
-    int maxDistance(vector<vector<int>>& grid) {
-        
-    }
-};
+int main()
+{
+    class Solution
+    {
+    public:
+        int maxDistance(vector<vector<int>> &grid)
+        {
+
+        }
+    };
+
+    vector<vector<int>> grid1 = {{1, 0, 1}, {0, 0, 0}, {1, 0, 1}};
+    vector<vector<int>> grid2={{1,0,0},{0,0,0},{0,0,0}};
+    Solution solution;
+    int test1 = solution.maxDistance(grid1);
+    int test2 = solution.maxDistance(grid2);
+    cout << "test1:" << test1 << endl;
+    cout << "test2:" << test2 << endl;
+    return 0;
+}
